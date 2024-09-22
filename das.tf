f120c1fccde3ee8c6e52500475205417c9c357a1
Van-thail <www.gorcnakan@gmail.com>
2024-09-22 19:24:56 +0400

    Update das.tf

--- das.tf
+++ das.tf
@@ -8,4 +8,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo echo '<center><h1>This is Matts instance that is successfully running the Apache Webserver!</h1></center>' > /var/www/html/index.html
+sudo echo '<center><h1>This is Jenkins!</h1></center>' > /var/www/html/index.html
