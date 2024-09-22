3f2389e98fccf11e32a0ae44d2064affb8b9efde
Van-thail <www.gorcnakan@gmail.com>
2024-09-22 19:24:24 +0400

    Update das.tf

--- das.tf
+++ das.tf
@@ -2,3 +2,10 @@ variable "environment" {
   type    = string
   default = "production"
 }
+
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo '<center><h1>This is Matts instance that is successfully running the Apache Webserver!</h1></center>' > /var/www/html/index.html
